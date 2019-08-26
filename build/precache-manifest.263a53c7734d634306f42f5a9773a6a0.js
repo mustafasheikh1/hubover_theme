e29f97ae51f34370c48e832bf280aaadcdeb6c06
mustafasheikh1 <mustafaahmed726@gmail.com>
2019-08-27 01:35:40 +0500

    changes

---
+++ build/precache-manifest.263a53c7734d634306f42f5a9773a6a0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d3cccf68b7cb3f311cc3f8b761f664b",
+    "url": "/static/media/server1.8d3cccf6.png"
+  },
+  {
+    "revision": "724896fb0ec003d9c729",
+    "url": "/static/css/main.07bc098c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8154ac8b48582abfd598",
+    "url": "/static/js/2.7ba72df0.chunk.js"
+  },
+  {
+    "revision": "7bcfe7c4520529e219dae4de16ad76c7",
+    "url": "/static/media/header-bg-2.7bcfe7c4.jpg"
+  },
+  {
+    "revision": "c24b6b9c0fcd84c7b258879880472660",
+    "url": "/static/media/Firebase.c24b6b9c.png"
+  },
+  {
+    "revision": "80687d23d8eef70e255e1996a06d9f5c",
+    "url": "/static/media/react.80687d23.png"
+  },
+  {
+    "revision": "724896fb0ec003d9c729",
+    "url": "/static/js/main.b6c8d126.chunk.js"
+  },
+  {
+    "revision": "9c47b6b3ff7c89bbcfcb2b64d57af2f0",
+    "url": "/static/media/Hubover.9c47b6b3.png"
+  },
+  {
+    "revision": "ddab5779128d209ad2a9410817fd5f15",
+    "url": "/static/media/hexagon.ddab5779.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "8154ac8b48582abfd598",
+    "url": "/static/css/2.6fbeebe2.chunk.css"
+  },
+  {
+    "revision": "349feadce1fccb618500cf7c5df88f14",
+    "url": "/index.html"
+  }
+];
